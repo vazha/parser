7cbc893564379cb2c8c45b7e1b5742bb9f09dc6f
vazha <grombl3@yahoo.com>
2015-12-14 23:18:04 +0200

    Сохранение1

---
+++ www/templates/emptyPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <style>
+table {
+border-top: 3px solid grey;
+border-bottom: 3px solid grey;
+border-spacing: 0px;
+}
+th, td {
+    border: 1px solid grey;
+    padding: 3px;
+}
+        </style> 
+        <title>[[title]]</title>
+    </head>
+    <body>
+    [[table]]    
+    </body>
+</html>
